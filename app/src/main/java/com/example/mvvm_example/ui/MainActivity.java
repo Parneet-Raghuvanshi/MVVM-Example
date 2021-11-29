640206ef428675582a2401ca61adb65a40895b63
Parneet <parneetraghuvanshi@gmail.com>
2021-11-29 23:48:56 +0530

    Created MainActivity with RecyclerView

--- app/src/main/java/com/example/mvvm_example/ui/MainActivity.java
+++ app/src/main/java/com/example/mvvm_example/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mvvm_example.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,9 +9,13 @@ import com.example.mvvm_example.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv_quotes);
     }
 }
