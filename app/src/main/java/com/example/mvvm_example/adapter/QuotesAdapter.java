583a7c705816edfe8febb0bd0f42f67b0336b24e
Parneet <parneetraghuvanshi@gmail.com>
2021-11-30 00:07:14 +0530

    Created Adapter for Quote RecyclerView

--- app/src/main/java/com/example/mvvm_example/adapter/QuotesAdapter.java
+++ app/src/main/java/com/example/mvvm_example/adapter/QuotesAdapter.java
@@ -1,4 +1,56 @@
 package com.example.mvvm_example.adapter;
 
-public class QuotesAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mvvm_example.R;
+import com.example.mvvm_example.models.QuoteModel;
+
+import java.util.List;
+
+public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.MyViewHolder>{
+
+    private Context context;
+    private List<QuoteModel> quotes;
+
+    public QuotesAdapter(Context context, List<QuoteModel> quotes) {
+        this.context = context;
+        this.quotes = quotes;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.quote_layout,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.quoteText.setText(quotes.get(position).getQuoteText());
+        holder.quoteAuthor.setText(quotes.get(position).getQuoteAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return quotes.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView quoteText;
+        public TextView quoteAuthor;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            quoteText = itemView.findViewById(R.id.quote_text);
+            quoteAuthor = itemView.findViewById(R.id.quote_author);
+        }
+    }
 }
