fec4451a1fa42067481267b3c3a6508d8f47ed44
Parneet <parneetraghuvanshi@gmail.com>
2021-11-30 00:47:22 +0530

    Created Main View

--- app/src/main/java/com/example/mvvm_example/ui/MainActivity.java
+++ app/src/main/java/com/example/mvvm_example/ui/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.mvvm_example.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.mvvm_example.R;
+import com.example.mvvm_example.adapter.QuotesAdapter;
+import com.example.mvvm_example.models.QuoteModel;
+import com.example.mvvm_example.viewmodel.QuotesViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private QuotesAdapter quotesAdapter;
+    private List<QuoteModel> quotes = new ArrayList<>();
+    private QuotesViewModel viewModel;
+    int page = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +32,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.rv_quotes);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        quotesAdapter = new QuotesAdapter(this,quotes);
+        recyclerView.setAdapter(quotesAdapter);
+
+        viewModel = new ViewModelProvider(this).get(QuotesViewModel.class);
+        viewModel.getQuotes(page).observe(this, new Observer<List<QuoteModel>>() {
+            @Override
+            public void onChanged(List<QuoteModel> quoteModels) {
+                if (quoteModels != null){
+                    quotes.addAll(quoteModels);
+                    quotesAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mvvm_example/viewmodel/QuotesViewModel.java
+++ app/src/main/java/com/example/mvvm_example/viewmodel/QuotesViewModel.java
@@ -1,4 +1,23 @@
 package com.example.mvvm_example.viewmodel;
 
-public class QuotesViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.mvvm_example.models.QuoteModel;
+import com.example.mvvm_example.repo.QuotesRepo;
+
+import java.util.List;
+
+public class QuotesViewModel extends ViewModel {
+    private MutableLiveData<List<QuoteModel>> quotes;
+    private QuotesRepo quotesRepo;
+
+    public QuotesViewModel() {
+    }
+
+    public MutableLiveData<List<QuoteModel>> getQuotes(int page) {
+        quotesRepo = new QuotesRepo(page);
+        quotes = quotesRepo.getQuotes();
+        return quotes;
+    }
 }
