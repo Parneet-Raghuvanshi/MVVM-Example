baeb71e4b7ca38c5586e854b29d24def03416469
Parneet <parneetraghuvanshi@gmail.com>
2021-11-29 23:39:27 +0530

    Completed Retrofit Code + Api (Class to Interface)

--- app/src/main/java/com/example/mvvm_example/retrofit/Api.java
+++ app/src/main/java/com/example/mvvm_example/retrofit/Api.java
@@ -1,4 +1,11 @@
 package com.example.mvvm_example.retrofit;
 
-public class Api {
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+    
+    @GET("quotes")
+    Call<ResponseBody> getQuotesApi();
 }
